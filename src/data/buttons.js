43c8f7fa71690cd90d168cdfa8952ffb7d7e255c
Jhair-Mobup <jlescano@mobuptacna.com>
2025-12-06 23:34:27 -0500

    feat: add initial button data for external links

--- src/data/buttons.js
+++ src/data/buttons.js
@@ -3,38 +3,38 @@ export const listaDeBotones = [
         id: 1,
         texto: 'Vida y Ministerio Cristianos',
         color: '#4285F4', // Azul Google
-        url: 'https://docs.google.com/document/d/1r759D7NykShdrWksz6wkgzTeaekriy4EFlw54jbFH0Q/preview?tab=t.0'
+        url: 'https://docs.google.com/document/d/1r759D7NykShdrWksz6wkgzTeaekriy4EFlw54jbFH0Q/preview'
     },
     {
         id: 2,
         texto: 'Discursos Públicos',
         color: '#34A853', // Verde Google
-        url: 'https://docs.google.com/spreadsheets/d/1_tx3ztvOFXFXHqqcL5NysbSy8AWQZilPEYwFB5hT4eY/preview?usp=sharing'
+        url: 'https://docs.google.com/spreadsheets/d/1_tx3ztvOFXFXHqqcL5NysbSy8AWQZilPEYwFB5hT4eY/preview'
     },
     {
         id: 3,
         texto: 'Acomodadores / Micrófonos',
         color: '#EA4335', // Rojo Google
-        url: 'https://docs.google.com/document/d/OTRO_DOC/preview'
+        url: 'https://docs.google.com/spreadsheets/d/1-Js7kfW2Kd-6zUa6-ljtTk-HGNY7QLG7G49zqGVMKIg/preview'
     },
     {
         id: 4,
-        texto: 'Anuncios Importantes',
-        color: '#FBBC05', // Amarillo Google
-        textoOscuro: true, // Para que la letra sea negra en fondo amarillo
-        url: 'https://docs.google.com/document/d/OTRO_DOC_MAS/preview'
-    },
-    {
-        id: 5,
         texto: 'Servicio del Campo',
         color: '#4285F4', // Azul Google
         textoOscuro: true, // Para que la letra sea negra en fondo amarillo
-        url: 'https://docs.google.com/spreadsheets/d/1hCugqSpqz__vdScZk9ItBar0PLPUdbTDfSUeoTn__qo/preview?gid=0#gid=0'
+        url: 'https://docs.google.com/spreadsheets/d/1hCugqSpqz__vdScZk9ItBar0PLPUdbTDfSUeoTn__qo/preview'
     },
     {
-        id: 6,
+        id: 5,
         texto: 'Informe de Predicación',
         color: '#373d46ff', // Gris Google
         url: 'https://docs.google.com/forms/d/e/1FAIpQLScqwBigXSjsTKnfhLhv03q-BwsqNReDY8N0_d0bYuY_rs9r7g/viewform'
-    }
+    },
+    {
+        id: 6,
+        texto: 'Solicitud de Precursor Auxiliar',
+        color: '#FBBC05', // Amarillo Google
+        textoOscuro: true, // Para que la letra sea negra en fondo amarillo
+        url: 'https://docs.google.com/forms/d/e/1FAIpQLSeXptFkt18Pd7LJ4LStjs_jrAoVarXAHzw7ieFBKBOKilQwzQ/viewform'
+    },
 ];
